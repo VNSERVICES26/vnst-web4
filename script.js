450ca9a1c5fc267db01a68f986f77392e5060602
VNSERVICES26 <vnetworkservices26@gmail.com>
2025-06-07 02:12:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,23 @@ function toggleMenu() {
 
 // VNST Swap integration
 function openSwap() {
-  const swapWindow = window.open('', 'vnst
+  const swapWindow = window.open('', 'vnstSwap', 'width=400,height=600');
+  swapWindow.document.write(`
+    <html>
+      <head>
+        <title>Buy VNST</title>
+        <style>
+          body, html { margin: 0; padding: 0; height: 100%; }
+          iframe { width: 100%; height: 100%; border: none; }
+        </style>
+      </head>
+      <body>
+        <iframe src="https://vnservices26.github.io/vnst-SWAP/"></iframe>
+      </body>
+    </html>
+  `);
+}
+
 
   menuToggle.addEventListener('click', () => {
     menu.classList.toggle('show');
