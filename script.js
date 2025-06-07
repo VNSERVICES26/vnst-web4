480406ca9552ddbf0ff1e6afa52e9153d4593360
VNSERVICES26 <vnetworkservices26@gmail.com>
2025-06-07 10:40:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('open');
+}
+
+function openSwap() {
+  const section = document.getElementById('swap-section');
+  section.classList.toggle('hidden');
+}
