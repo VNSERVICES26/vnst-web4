fd699f4039fb16318ed0a6d7b30da322bc15e8e2
VNSERVICES26 <vnetworkservices26@gmail.com>
2025-06-07 10:38:27 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  const menu = document.getElementById("menu");
-  menu.classList.toggle("show");
-}
-
-function openSwap() {
-  const swapSection = document.getElementById("swap-section");
-  swapSection.scrollIntoView({ behavior: "smooth" });
-}
